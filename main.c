f19e9420cb78cf96d6c5cc389a3e9eee6da6ae9b
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-09-20 09:34:55 +0530

    Update main.c

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ int readParagraph() {
 
 int readWord() {
     char Word[30];
-    printf("\n Enter the word  to be counted: ");
+    printf("Enter the word  to be counted: ");
     gets(Word);
     return Word;
 }
